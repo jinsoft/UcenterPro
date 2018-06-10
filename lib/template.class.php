87cb670207d33452cb138e6ec5f4f89b4f50cb06
xiao <jx_xiaojin@aliyun.com>
2018-06-10 19:40:05 +0800

    修改模板引擎匹配规则

--- lib/template.class.php
+++ lib/template.class.php
@@ -72,47 +72,77 @@ class template {
 	}
 
 	function complie() {
-		$template = file_get_contents($this->tplfile);
-		$template = preg_replace("/\<\!\-\-\{(.+?)\}\-\-\>/s", "{\\1}", $template);
-		$template = preg_replace("/\{lang\s+(\w+?)\}/ise", "\$this->lang('\\1')", $template);
+        $template = file_get_contents($this->tplfile);
+        $template = preg_replace("/\<\!\-\-\{(.+?)\}\-\-\>/s", "{\\1}", $template);
+        $template = preg_replace_callback("/\{lang\s+(\w+?)\}/is", function($r) {
+            return $this->lang($r[1]);
+        },$template);
 
-		$template = preg_replace("/\{($this->var_regexp)\}/", "<?=\\1?>", $template);
-		$template = preg_replace("/\{($this->const_regexp)\}/", "<?=\\1?>", $template);
-		$template = preg_replace("/(?<!\<\?\=|\\\\)$this->var_regexp/", "<?=\\0?>", $template);
+        $template = preg_replace("/\{($this->var_regexp)\}/", "<?=\\1?>", $template);
+        $template = preg_replace("/\{($this->const_regexp)\}/", "<?=\\1?>", $template);
+        $template = preg_replace("/(?<!\<\?\=|\\\\)$this->var_regexp/", "<?=\\0?>", $template);
 
-		$template = preg_replace("/\<\?=(\@?\\\$[a-zA-Z_]\w*)((\[[\\$\[\]\w]+\])+)\?\>/ies", "\$this->arrayindex('\\1', '\\2')", $template);
+        $template = preg_replace_callback("/\<\?=(\@?\\\$[a-zA-Z_]\w*)((\[[\\$\[\]\w]+\])+)\?\>/is", function($r) {
+            return $this->arrayindex($r[1], $r[2]);
+        },$template);
 
-		$template = preg_replace("/\{\{eval (.*?)\}\}/ies", "\$this->stripvtag('<? \\1?>')", $template);
-		$template = preg_replace("/\{eval (.*?)\}/ies", "\$this->stripvtag('<? \\1?>')", $template);
-		$template = preg_replace("/\{for (.*?)\}/ies", "\$this->stripvtag('<? for(\\1) {?>')", $template);
+        $template = preg_replace_callback("/\{\{eval (.*?)\}\}/is", function($r) {
+            return $this->stripvtag("<? $r[1]?>");
+        },$template);
 
-		$template = preg_replace("/\{elseif\s+(.+?)\}/ies", "\$this->stripvtag('<? } elseif(\\1) { ?>')", $template);
-
-		for($i=0; $i<2; $i++) {
-			$template = preg_replace("/\{loop\s+$this->vtag_regexp\s+$this->vtag_regexp\s+$this->vtag_regexp\}(.+?)\{\/loop\}/ies", "\$this->loopsection('\\1', '\\2', '\\3', '\\4')", $template);
-			$template = preg_replace("/\{loop\s+$this->vtag_regexp\s+$this->vtag_regexp\}(.+?)\{\/loop\}/ies", "\$this->loopsection('\\1', '', '\\2', '\\3')", $template);
-		}
-		$template = preg_replace("/\{if\s+(.+?)\}/ies", "\$this->stripvtag('<? if(\\1) { ?>')", $template);
-
-		$template = preg_replace("/\{template\s+(\w+?)\}/is", "<? include \$this->gettpl('\\1');?>", $template);
-		$template = preg_replace("/\{template\s+(.+?)\}/ise", "\$this->stripvtag('<? include \$this->gettpl(\\1); ?>')", $template);
+        $template = preg_replace_callback("/\{eval (.*?)\}/is", function($r) {
+            return $this->stripvtag("<? $r[1]?>");
+        },$template);
 
 
-		$template = preg_replace("/\{else\}/is", "<? } else { ?>", $template);
-		$template = preg_replace("/\{\/if\}/is", "<? } ?>", $template);
-		$template = preg_replace("/\{\/for\}/is", "<? } ?>", $template);
+        $template = preg_replace_callback("/\{for (.*?)\}/is", function($r) {
+            return $this->stripvtag("<? for ($r[1]) {?>");
+        },$template);
 
-		$template = preg_replace("/$this->const_regexp/", "<?=\\1?>", $template);
+        $template = preg_replace_callback("/\<\?=(\@?\\\$[a-zA-Z_]\w*)((\[[\\$\[\]\w]+\])+)\?\>/is", function($r) {
+            return $this->arrayindex($r[1], $r[2]);
+        },$template);
 
-		$template = "<? if(!defined('UC_ROOT')) exit('Access Denied');?>\r\n$template";
-		$template = preg_replace("/(\\\$[a-zA-Z_]\w+\[)([a-zA-Z_]\w+)\]/i", "\\1'\\2']", $template);
+        $template = preg_replace_callback("/\{elseif\s+(.+?)\}/is", function($r) {
+            return $this->stripvtag("<? } elseif($r[1]) { ?>");
+        },$template);
 
-		$template = preg_replace("/\<\?(\s{1})/is", "<?php\\1", $template);
-		$template = preg_replace("/\<\?\=(.+?)\?\>/is", "<?php echo \\1;?>", $template);
+        for($i=0; $i<2; $i++) {
+            $template = preg_replace_callback("/\{loop\s+$this->vtag_regexp\s+$this->vtag_regexp\s+$this->vtag_regexp\}(.+?)\{\/loop\}/is", function($r) {
+                return $this->loopsection($r[1], $r[2], $r[3], $r[4]);
+            },$template);
 
-		$fp = fopen($this->objfile, 'w');
-		fwrite($fp, $template);
-		fclose($fp);
+            $template = preg_replace_callback("/\{loop\s+$this->vtag_regexp\s+$this->vtag_regexp\}(.+?)\{\/loop\}/is", function($r) {
+                return $this->loopsection($r[1], '', $r[2], $r[3]);
+            },$template);
+
+        }
+
+        $template = preg_replace_callback("/\{if\s+(.+?)\}/is", function($r) {
+            return $this->stripvtag("<? if($r[1]) { ?>");
+        },$template);
+
+        $template = preg_replace("/\{template\s+(\w+?)\}/is", "<? include \$this->gettpl('\\1');?>", $template);
+
+        $template = preg_replace_callback("/\{template\s+(.+?)\}/is", function($r) {
+            return $this->stripvtag("<? include $this->gettpl($r[1]); ?>");
+        },$template);
+
+        $template = preg_replace("/\{else\}/is", "<? } else { ?>", $template);
+        $template = preg_replace("/\{\/if\}/is", "<? } ?>", $template);
+        $template = preg_replace("/\{\/for\}/is", "<? } ?>", $template);
+
+        $template = preg_replace("/$this->const_regexp/", "<?=\\1?>", $template);
+
+        $template = "<? if(!defined('UC_ROOT')) exit('Access Denied');?>\r\n$template";
+        $template = preg_replace("/(\\\$[a-zA-Z_]\w+\[)([a-zA-Z_]\w+)\]/i", "\\1'\\2']", $template);
+
+        $template = preg_replace("/\<\?(\s{1})/is", "<?php\\1", $template);
+        $template = preg_replace("/\<\?\=(.+?)\?\>/is", "<?php echo \\1;?>", $template);
+
+        $fp = fopen($this->objfile, 'w');
+        fwrite($fp, $template);
+        fclose($fp);
 	}
 
 	function arrayindex($name, $items) {
